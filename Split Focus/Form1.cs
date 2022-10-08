20b5dbfdbd66372b7aa02eb14d7714d42494cf40
Elliot Melville <elliot@melville.org.nz>
2022-10-08 16:57:51 +1300

    Add project files.

---
+++ Split Focus/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Split_Focus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Rectangle rec = new Rectangle(0, 0, 40, 40);
+            DrawRectangle(pen, rec);
+        }
+
+    }
+}
